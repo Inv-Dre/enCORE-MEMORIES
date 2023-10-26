164409fc526659f4ce8888ae24fb0f49ce966b29
Dre Tay <andretaylor681@gmail.com>
2023-10-26 19:29:07 -0400

    add an Update thought, create reaction, and delete reaction function to the  thought controller.

--- Main/controller/thoughtsController.js
+++ Main/controller/thoughtsController.js
@@ -1,9 +1,10 @@
 const Thoughts = require('../models/thoughts');
+const User = require('../models/users')
 
 module.exports = {
     async getThoughts(req, res) { 
         try{
-        const thoughts = Thoughts.find();
+        const thoughts = await Thoughts.find();
         res.json(thoughts);
         } catch (err){
             console.log(err);
@@ -12,7 +13,7 @@ module.exports = {
     },
     async oneThought(req,res) {
         try{
-        const thought = Thoughts.findById({_id: req.params.userId})
+        const thought = await Thoughts.findById({_id: req.params.userId})
         .select('-__v')
         .populate('reactions');
 
@@ -26,8 +27,12 @@ module.exports = {
     },
     async createThought (req,res){
         try{
-            const thoughtData = Thoughts.create(req.body)
-            console.log(thoughtData)
+            const {thoughtsText, username} = req.body
+            const thoughtData = await Thoughts.create({thoughtsText, username});
+            console.log(thoughtData);
+            const user = await User.findOne({username: username});
+            await user.thoughts.push().save();
+            console.log(user);
                 res.send("Thought Created");
         } catch (err){
             res.json(500).json(err);
@@ -35,12 +40,57 @@ module.exports = {
     },
     async deleteThought(req,res){
         try{
-            const destroyThought = Thoughts.deleteOne({_id: req.params.thoughtId})
+            const destroyThought = await Thoughts.deleteOne({_id: req.params.thoughtId})
             console.log(destroyThought)
             res.send('thought deleted');
         } catch (err){
             res.json(500).json(err);
         }
     },
+    async updateThought(req,res){
+        try{
+            const thought = await Thoughts.findOne({_id:req.params.thoughtId}) => {
+                console.log(thought);  
+            };
+            console.log(thought);
+            res.send('Thought Updated');
 
+        }catch(err){
+            res.json(500).json(err);
+        }
+    },
+
+    async createReaction (req,res){
+        try{
+            const thought = await Thoughts.findOne({_id:req.params.thoughId})
+            if(!thought){
+               res.send('thought not found')
+            }
+            const reaction = req.body
+
+            const newReaction = thought.reactions.push(reaction).save();
+            console.log(newReaction);
+            res.send('Reaction created');
+        }catch (err){
+            res.json(err)
+        }
+    },
+
+    async deleteReaction(req,res){
+        try{
+            const {thoughtId, reactionId} = req.params
+            const thought = await Thoughts.findOne({_id: thoughtId});
+            const reaction = await Thoughts.findOne({_id:reactionId});
+            if(!thought || !reaction){
+                res.send('thought or reaction not found')
+            }
+            const deletedReaction = thought.reactions.splice(reaction);
+            console.log(deletedReaction);
+            res.send('reaction deleted');
+           
+        }catch (err) {
+            res.status(500).json(err);
+        }
+    },
 }
+
